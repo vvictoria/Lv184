97bf162833ddbef7839d3d23e9138044a91ca128
vvictoria <viniarska.v@gmail.com>
2016-05-23 02:16:34 +0300

    first commit

---
+++ 2.express+jade/config/config.js
@@ -0,0 +1,5 @@
+exports.port = 3000;
+exports.address = '0.0.0.0';
+exports.sayHello = function(name) {
+    console.log('Hello, ' + name + '!');
+};
---
+++ 2.express+jade/express.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+// var app = require('express')();
+
+var config = require('./config/config.js');
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('main', { title: 'Main page', message: 'Hello there!'});
+});
+
+app.get('/about', function (req, res) {
+  res.render('main', { title: 'About us', message: 'We are developers.'});
+});
+
+app.get('/contact', function (req, res) {
+  res.render('main', { title: 'Contact', message: 'Our phone is 555-1234'});
+});
+
+
+
+app.listen(config.port, config.address, function () {
+  console.log('Example app listening on port ' + config.port + '!');
+});
+
+
+// app.listen(port, onFinishedCallback);
+// app.listen(port, host, onFinishedCallback);
